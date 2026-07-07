13e283dd7eb807229450a5c98be3b83947cc0bcf
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 11:25:01 +0000

    Hausla

---
+++ BLIZZ/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //all codes in newer version of JS
+
+//alert(3+3) //here its node.js and this thing will work in browser
+
+console.log(3+3); 
+console.log("Jugnu");
+
+let age = 3;
+console.log(typeof(age));
+ 
+/* DATATYPES -  
+1. Number
+2. String " idhr likh "
+3. Boolean 
+4. null -> stand alone value
+5. undefined
+6. symbol
+*/
+
+
+/*OBJECT - 
+typeof(x) ->  x ka type btata haii
+*/
