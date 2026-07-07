bb50e183874196ca70a208e1c2a71b7304242060
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 12:07:05 +0000

    Hausla

--- BLIZZ/03_conversions.js
+++ BLIZZ/03_conversions.js
@@ -21,3 +21,26 @@ console.log(boolIsLogged); //this gives 1 ka boolean value i.e. (true)
 // 0->false
 // "" empty string -> false
 // "kuchh bhi not empty string" -> true
+
+
+
+//************************ Operations **************************
+
+
+let val  = 300
+let negVal = -val
+//console.log(negVal);
+
+let a ="blehh "
+let b ="sexyy "
+//console.log(a+b); 
+
+//String related stuff
+
+//going from left to right ----->
+console.log("1" + 2 + 3) //1st wala is string so sbko string hi consider =>123
+console.log(1 + 2 + "3") //1st and 2nd ko number consider and last ko string => 33
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1)
