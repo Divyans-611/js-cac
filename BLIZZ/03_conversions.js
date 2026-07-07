0090adf6b0cb6db76c3cc6f06a887afe7ce890af
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 11:31:48 +0000

    Hausla

---
+++ BLIZZ/03_conversions.js
@@ -0,0 +1,10 @@
+let score = "30" //this is string
+console.log(typeof(score));
+
+
+//now to convert it to number
+//datatype written in uppercase hhere "Number(x) " converts string to number
+let valInNumber = Number(score);
+console.log(valInNumber);
+console.log(typeof(valInNumber));
+
