6780f16d1885adaefa5f51f55a8a93740d2ca195
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 11:05:49 +0000

    yupp

--- BLIZZ/lett.js
+++ BLIZZ/lett.js
@@ -1,2 +1,12 @@
 let a = "Chutiyaa";
 console.log(a);
+
+
+const name= "Divv";
+// name="Fck"; ni hoga change
+console.log(name);
+
+var city = "Gharrr jana h";
+console.table([a,name,city]);
+
+// var nhi use kro -> functional scope mein prblm
