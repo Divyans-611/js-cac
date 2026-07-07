a3765955493e5f7b1cd0bcb7b00959b3aa332786
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 12:10:43 +0000

    Hausla

--- BLIZZ/04_comparisons.js
+++ BLIZZ/04_comparisons.js
@@ -2,3 +2,10 @@
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 != 1);
+
+
+//make sure the datatypes are same
+
+console.log("2" > 1);
+console.log(null == 0);
+console.log(null >= 0);
