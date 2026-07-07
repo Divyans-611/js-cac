3db8636beb98e580cb7b3666f95b0f9cf521cbc7
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-07 11:40:10 +0000

    Hausla

--- BLIZZ/03_conversions.js
+++ BLIZZ/03_conversions.js
@@ -8,3 +8,8 @@ let valInNumber = Number(score);
 console.log(valInNumber);
 console.log(typeof(valInNumber));
 
+/* NaN ->  Not a Number */
+
+// "30" string => 30 number
+// "30abss" string => NaN ->  Not a Number bitchh
+
