e5e864f9dead2ee2cd32403b50f95adc6cbc51c4
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-08 06:54:37 +0000

    eendhan

---
+++ BLIZZ/datatype_summ.js
@@ -0,0 +1,19 @@
+// pass by reference (Non-primitive)
+
+// Arrays, Objects, Function
+
+const heroes = ["Shaktimaan","Bhaiuns","Ki","Taint"];
+//object
+let blehh = {
+    name: "Lol",
+    age: 23,
+}
+
+console.log(heroes[1]);
+console.table(heroes);
+
+//functions
+function plio(){
+    console.log("Hizrrr");
+}
+plio();
