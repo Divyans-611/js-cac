0dad22c2bd5e3009fe95d116b90cb61058482a5b
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-08 06:48:39 +0000

    eendhan

--- BLIZZ/04_comparisons.js
+++ BLIZZ/04_comparisons.js
@@ -9,3 +9,10 @@
 console.log("2" > 1);
 console.log(null == 0);
 console.log(null >= 0);
+//comparisons(>,<,>=,<=) convert null to a number and treat it as 0
+//isliye yha (null >= 0) will give us true 
+
+//STRICT CHECK === -> values and datatype dono check kregaa
+
+console.log("2" == 2); //yehh sirf values check krega
+console.log("2" === 2); //yehh datatype bhi and false dega
