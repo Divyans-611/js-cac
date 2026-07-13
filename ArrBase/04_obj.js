1d983046de826241ae34387bc31e590ec6b312a9
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-13 09:48:17 +0000

    json yeahh

---
+++ ArrBase/04_obj.js
@@ -0,0 +1,26 @@
+//Object de-structure
+const course = {
+    courseName : "JS",
+    courseTime : "Chhutti",
+    courseIns  : "hiteshhh"
+}
+const {courseIns : instructor} = course
+console.log(instructor);
+
+
+// const navbar = () => {
+     
+// }
+
+//*************** json API ****************
+
+
+//keys and values both are string
+
+// {
+//     "name"   : "Hill",
+//     "course" : "JS",
+//     "price"  : "Free"
+// }
+
+
