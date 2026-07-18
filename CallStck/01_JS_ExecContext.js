7a5e7fd2304b1e3108cf342963bb36ee5d7b9b88
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-18 10:53:04 +0000

    Call Stack

---
+++ CallStck/01_JS_ExecContext.js
@@ -0,0 +1,10 @@
+//Javascript has Execution Contexts
+
+/*
+    1. Global Execution Context
+    2. Function Execution Context
+    3. Eval Execution Context...
+*/
+/* - Memory creation phase -> memory allocate ki jaati hai isme sirf...
+   - Execution phase...
+*/
