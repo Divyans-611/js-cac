2af36e28bc72f73bca3f4290311fefb57438bc9e
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-18 07:37:17 +0000

    obj nd arr

--- Fns/01_fxn.js
+++ Fns/01_fxn.js
@@ -11,7 +11,41 @@ naam(); //function reference
 
 
 function loginUsr(username){
-    return `${username} logged in`
-    
+    return `${username} logged in`    
 }
-console.log(loginUsr("Jeff"));
+//console.log(loginUsr("Jeff"));
+
+
+// ************REST/SPREAD Operator(...num2)*************
+function CartPrice(...num1){
+    return num1;
+}
+
+console.log(CartPrice(300,400,500));
+
+
+//to acess object using func
+const user = {
+    username : "halkuu",
+    price : 3432
+}
+
+function accessObj(anyObj){
+    console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`);
+}
+accessObj(user);
+
+//direct bhi object pass kr skte h function mein
+accessObj({
+    username : "sksks",
+    price : 233
+})
+
+
+//passing arr
+const arr = [200,45,1924,245]
+function returnSecond(array){
+    return array[1];
+}
+
+console.log("Second element of the array is : ", returnSecond(arr));
