6ca322d85851120a6f73073e97aa4f469b2620f0
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-18 05:19:11 +0000

    fxxn

---
+++ Fns/01_fxn.js
@@ -0,0 +1,17 @@
+function naam(){
+    console.log("Eyahhhhh")
+}
+naam(); //function reference
+
+// function addTwoNumbers(a,b){
+//     return (a+b);
+// }
+// const sum = addTwoNumbers(22,88);
+// console.log(sum);
+
+
+function loginUsr(username){
+    return `${username} logged in`
+    
+}
+console.log(loginUsr("Jeff"));
