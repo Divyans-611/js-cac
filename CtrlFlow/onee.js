dccc8bea44b83e61ee918f123707e0a1aa95a884
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-19 16:30:11 +0000

    HEB

---
+++ CtrlFlow/onee.js
@@ -0,0 +1,10 @@
+//falsy values
+
+false, 0 , -0 , BigInt, 0n, "" , null ,undefined, NaN // these are false values and other are true
+
+
+//truthy values
+
+"0", 'false'-> string k andar likhe h, " ",
+[], {}, function(){}
+
