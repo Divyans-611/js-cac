a56e404b4e8608dedf3375e6b04f9909b62dc3e5
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-19 16:40:58 +0000

    hmm

--- CtrlFlow/onee.js
+++ CtrlFlow/onee.js
@@ -1,10 +1,25 @@
-//falsy values
+// //falsy values
 
-false, 0 , -0 , BigInt, 0n, "" , null ,undefined, NaN // these are false values and other are true
+// false, 0 , -0 , BigInt, 0n, "" , null ,undefined, NaN // these are false values and other are true
 
 
-//truthy values
+// //truthy values
 
-"0", 'false'-> string k andar likhe h, " ",
-[], {}, function(){}
+// "0", 'false'-> string k andar likhe h, " ",
+// [], {}, function(){}
 
+//************* Nullish Coalescing Operator (??) : null , undefined************
+let val1;
+//val1 = 5 ?? 10 // gives 5
+//val1 = null ?? 10 // in this case gives 10
+//val1 = undefined ?? 100 //in this case gives 100
+
+val1 = undefined ?? 10 ?? 20 ?? 30 //returns 10 ->first value after null/undefined
+
+console.log(val1);
+
+
+//*************TERNARY OPERAATOR ******************
+
+let x = 50
+x > 100 ? console.log("Niceee") : console.log("Bhains ki taint")
