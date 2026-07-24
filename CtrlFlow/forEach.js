def20d4a5568cfa8374233dc5c55f863c4d7bff2
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-24 21:42:24 +0000

    Bhains ki taint

---
+++ CtrlFlow/forEach.js
@@ -0,0 +1,15 @@
+const coding = ["app","js","cpp","rb"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item)
+// } ) 
+// console.log(values)//undefined bcz forEach does not return anything to us...so explicitly we have undefined here
+
+//lets check this again by returning
+
+// const values = coding.forEach( (item) => {
+//     return item
+// } )
+// console.log(values) //yehh  bhi undefined hi rhega...iska matlab forEach hamesha kuchh return na krtaa hai
+
+
