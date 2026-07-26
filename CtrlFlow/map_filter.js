7386a27dcb8aa50b7b75c97e42e590ea68b62405
Divyans Srivastava <divysrivastav5@gmail.com>
2026-07-26 07:51:21 +0000

    filter

---
+++ CtrlFlow/map_filter.js
@@ -0,0 +1,3 @@
+const myNums = [1,2,3,4,5,67,8,9,10,11]
+const newNums = myNums.filter( (num) => num > 4 ) /////filter ke andar bhi callback funcn hi daal dete hain...
+console.log(newNums) //this will give us all numbers greater than 4 int hte arr
