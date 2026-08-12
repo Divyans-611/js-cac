6eb07ad1d0fc611e76d2732da4cb0519d7ea0e11
Divyans Srivastava <divysrivastav5@gmail.com>
2026-08-12 15:24:53 +0000

    dirty dom

--- CtrlFlow/map_filter.js
+++ CtrlFlow/map_filter.js
@@ -1,3 +1,13 @@
 const myNums = [1,2,3,4,5,67,8,9,10,11]
-const newNums = myNums.filter( (num) => num > 4 ) /////filter ke andar bhi callback funcn hi daal dete hain...
-console.log(newNums) //this will give us all numbers greater than 4 int hte arr
+// const newNums = myNums.filter( (num) => {
+//     return (num > 4)
+// } ) /////filter ke andar bhi callback funcn hi daal dete hain...
+// console.log(newNums) //this will give us all numbers greater than 4 in the arr
+
+const newNums = []
+myNums.forEach( (num) => {
+    if(num > 4){
+        newNums.push(num)
+    }
+})
+console.log(newNums)
